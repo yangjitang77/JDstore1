c7b9793695f0f06e6df6f8cee683f85fc848f6df
yangjitang77 <yangjitang77@gmail.com>
2017-06-06 12:31:04 +0800

    add intro-box and carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,14 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+
+
+/*===== 首页导览列变化特效 =====*/
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 125) {  //当画面高度至125px时触发效果
+		$('#navbar').addClass('scroll_navbar')  //navbar 增加 scroll_navbar CSS
+	} else {
+		$('#navbar').removeClass('scroll_navbar') //navbar 移除 scroll_navbar CSS
+	}
+})
